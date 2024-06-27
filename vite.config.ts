852d63d3d9903ccfd8b4149ed99b9c8b8504383a
AnnaMaria2711 <anna-maria.buelk@coop.ch>
2024-06-27 13:18:06 +0200

    coded a lil more more more more more

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/modul-293/', // This should be the name of your repository
+});
